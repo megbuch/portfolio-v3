61a62ffaa99789d3cabb3775ccac7af3d51cfd26
Meghan Bucher <115611931+megbuch@users.noreply.github.com>
2025-12-05 22:14:47 -0800

    Fix navigation placement.

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -19,13 +19,15 @@ export default function Navigation() {
 
   return (
     <div className='navigation-comp row'>
-      <div className='navigation-items row'>
-        {items.map(item => (
-          <div key={item.path} className={`item ${activeItem == item.path && 'active'}`} onClick={()=>navigate(item.path)}>{item.icon}</div>
-        ))}
+      <div className='navigation-container row'>
+        <div className='navigation-items row'>
+          {items.map(item => (
+            <div key={item.path} className={`item ${activeItem == item.path && 'active'}`} onClick={()=>navigate(item.path)}>{item.icon}</div>
+          ))}
+        </div>
+        <div className='side left'></div>
+        <div className='side right'></div>
       </div>
-      <div className='side left'></div>
-      <div className='side right'></div>
     </div>
   )
 }
